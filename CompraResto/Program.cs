21842647f86b53aee87ff20877d22631e01a815e
Vanidad <57409926+vanesa33@users.noreply.github.com>
2021-02-10 16:57:25 -0300

    CompraResto en Consola #
    Suma y multiplicación en consola, Práctica de Ejercicio para principiantes en C#

---
+++ CompraResto/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte CANB, CANH, CANP;
+            double APAGAR;
+            string linea;
+            const double PRECIOB = 0.8;
+            const double PRECIOH = 2;
+            const double PRECIOP = 1.2;
+            Console.Write("CANTIDAD DE HAMBURGUESAS :"); linea = Console.ReadLine();
+            CANH = byte.Parse(linea);
+            Console.Write("CANTIDAD DE PAPAS :"); linea = Console.ReadLine();
+            CANP = byte.Parse(linea);
+            Console.Write("CANTIDAD DE BEBIDAS :"); linea = Console.ReadLine();
+            CANB = byte.Parse(linea);
+            Console.WriteLine();
+            APAGAR = (CANH * PRECIOH) + (CANP * PRECIOP) + (CANB * PRECIOB);
+            Console.WriteLine("VALOR A PAGAR: " + APAGAR);
+            Console.Write("Pulse una Tecla:"); Console.ReadLine();
+        }
+    }
+}
